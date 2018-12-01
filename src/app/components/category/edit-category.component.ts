8cb80ed81ba8cbe29a35ebe012559f5d5ff624ae
Weverton Souza <ene@localhost.localdomain>
2018-12-01 19:21:39 -0200

    Criado o CRUD de categoria.

---
+++ src/app/components/category/edit-category.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../model/category';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CategoryService } from '../../services/category.service';
+import { Router } from '@angular/router';
+import {first} from 'rxjs/operators';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-edit-category',
+  templateUrl: './edit-category.component.html',
+  styles: []
+})
+export class EditCategoryComponent implements OnInit {
+
+  category: Category;
+  editForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private service: CategoryService) { }
+
+  ngOnInit() {
+
+    const idCategory = localStorage.getItem('editCategoryId');
+
+    if ( !idCategory ) {
+      alert('Ação inválida');
+      this.router.navigate(['list-category']);
+      return;
+    }
+
+    this.editForm = this.formBuilder.group({
+      id: [],
+      description: ['', Validators.required],
+      price: ['', Validators.required]
+    });
+
+    this.service.findById(idCategory)
+      .subscribe(data => {
+        this.editForm.setValue(data);
+      });
+  }
+
+  onSubmit() {
+    this.service.update(this.editForm.value)
+      .pipe(first())
+      .subscribe( data => {
+        this.router.navigate(['list-category']);
+        swal({
+          position: 'top',
+          type: 'success',
+          title: 'Categoria atualizada com sucesso!',
+          showConfirmButton: false,
+          timer: 1500
+        });
+      },
+      error => {
+        alert(error);
+      });
+  }
+
+}
---
+++ src/app/components/category/list-category.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../../model/category';
+import { CategoryService } from '../../services/category.service';
+import { Router } from '@angular/router';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-list-category',
+  templateUrl: './list-category.component.html',
+  styles: []
+})
+export class ListCategoryComponent implements OnInit {
+
+  categories: Category[];
+  constructor(private router: Router, private service: CategoryService) { }
+
+  ngOnInit() {
+    this.service.findAll().subscribe(data => (this.categories = data));
+  }
+
+  update(category: Category): void {
+    localStorage.removeItem('editCategoryId');
+    localStorage.setItem('editCategoryId', category.id);
+    this.router.navigate(['edit-category']);
+  }
+
+  add(): void {
+    this.router.navigate(['add-category']);
+  }
+
+}
